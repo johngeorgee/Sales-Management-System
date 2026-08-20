5ceed2d655edad8da80f26c52109de3b0efb804e
johngeorgee <johngeorgeea@gmail.com>
2026-08-20 06:20:45 +0300

    Finished Authentication & Authorization Implementation - From Backend to UX

--- Frontend/src/app/pages/Auth/Login/login.component.ts
+++ Frontend/src/app/pages/Auth/Login/login.component.ts
@@ -1,4 +1,4 @@
-// login.component.ts
+// pages/Auth/Login/login.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -18,31 +18,33 @@ export class LoginComponent {
   rememberMe: boolean = false;
   hidePassword: boolean = true;
   loading = false;
-  error: string = ''
-  constructor(private router: Router, private authServ: AuthService) {}
+  error: string = '';
+
+  constructor(
+    private router: Router,
+    private authServ: AuthService
+  ) {}
 
   onSubmit(): void {
-    if(!this.email || !this.password){
+    if (!this.email || !this.password) {
       this.error = 'Please enter email & Password';
       return;
     }
+
     this.loading = true;
     this.error = '';
+
     this.authServ.login(this.email, this.password).subscribe({
-      next: (response) =>{
-        console.log('Login Succesfully', response);
+      next: (response) => {
+        console.log('Login Successfully', response);
         this.loading = false;
-        this.router.navigate(['/dashboard'])
-      }, 
-      error:(err) => {
-        console.error('Login failed', err); 
-        this.loading = false; 
-        this.error = err.error?.message || 'Login failed. Please check your email and password.';
-        
+        this.router.navigate(['/dashboard']);
       },
-    })
-    }
-    
-    // Redirect to dashboard after successful login
-    // this.router.navigate(['/dashboard']);
+      error: (err) => {
+        console.error('Login failed', err);
+        this.loading = false;
+        this.error = err.error?.message || 'Login failed. Please check your email and password.';
+      }
+    });
   }
+}
