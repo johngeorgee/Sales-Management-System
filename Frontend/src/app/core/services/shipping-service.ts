297afd3b7b4d7fbd24759fd1f7ad8757a34943e4
johngeorgee <johngeorgeea@gmail.com>
2026-08-20 13:55:12 +0300

    Fixed Problems in shipping feature

--- Frontend/src/app/core/services/shipping-service.ts
+++ Frontend/src/app/core/services/shipping-service.ts
@@ -10,62 +10,62 @@ import { ApiResponse } from '../Models/api-response';
 export class ShippingService {
   private apiUrl = 'http://localhost:3000/shipping'
   constructor(private http: HttpClient) {
-    
-    
+
+
   }
 
-  
-    // Get all shippings
-    getShippings(page:number, limit:number){
-      return this.http.get<ShippingResponse>(
-        `${this.apiUrl}?page=${page}&limit=${limit}`
-      );
-    }
-  
-    // Get shipping by ID
-    getShippingById(id: string): Observable<{
+
+  // Get all shippings
+  getShippings(page: number, limit: number) {
+    return this.http.get<ShippingResponse>(
+      `${this.apiUrl}?page=${page}&limit=${limit}`
+    );
+  }
+
+  // Get shipping by ID
+  getShippingById(id: string): Observable<{
+    message: string;
+    data: IShipping;
+  }> {
+    return this.http.get<{
       message: string;
       data: IShipping;
-    }> {
-      return this.http.get<{
-        message: string;
-        data: IShipping;
-      }>(`${this.apiUrl}/${id}`);
-    }
-  
-    // Add shipping
-    addShipping(shipping: IShipping): Observable<{
+    }>(`${this.apiUrl}/${id}`);
+  }
+
+  // Add shipping
+  addShipping(shipping: IShipping): Observable<{
+    message: string;
+    data: IShipping;
+  }> {
+    return this.http.post<{
       message: string;
       data: IShipping;
-    }> {
-      return this.http.post<{
-        message: string;
-        data: IShipping;
-      }>(this.apiUrl, shipping);
-    }
-  
-    // Update shipping
-    updateShipping(
-      id: string,
-      shipping: Partial<IShipping>
-    ): Observable<{
+    }>(this.apiUrl, shipping);
+  }
+
+  // Update shipping
+  updateShipping(
+    id: string,
+    shipping: Partial<IShipping>
+  ): Observable<{
+    message: string;
+    data: IShipping;
+  }> {
+    return this.http.put<{
       message: string;
       data: IShipping;
-    }> {
-      return this.http.put<{
-        message: string;
-        data: IShipping;
-      }>(`${this.apiUrl}/${id}`, shipping);
-    }
-  
-    // Delete shipping
-    deleteShipping(id: string): Observable<{
+    }>(`${this.apiUrl}/${id}`, shipping);
+  }
+
+  // Delete shipping
+  deleteShipping(id: string): Observable<{
+    message: string;
+    success?: boolean;
+  }> {
+    return this.http.delete<{
       message: string;
       success?: boolean;
-    }> {
-      return this.http.delete<{
-        message: string;
-        success?: boolean;
-      }>(`${this.apiUrl}/${id}`);
-    }
+    }>(`${this.apiUrl}/${id}`);
   }
+}
